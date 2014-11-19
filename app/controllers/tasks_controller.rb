68224d9a758f7c1de3a919922f22430d391371fb
ruyielder <ruyielder@gmail.com>
2014-11-19 18:46:44 +0100

    Added inserting task's tags

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,11 +24,13 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+    update_tags
     respond_with(@task)
   end
 
   def update
     @task.update(task_params)
+    update_tags
     respond_with(@task)
   end
 
@@ -45,5 +47,34 @@ class TasksController < ApplicationController
     def task_params
       default_params = {author_id: current_user.id}
       params.require(:task).permit(:question, :answer).merge default_params
+    end
+
+    def update_tags
+      line = params[:task][:tags_line]
+      tag_names = get_tag_names(line)
+      tags = @task.user_tags
+      mapping_tags = {}
+      tags.each {|tag| mapping_tags[tag.tag] = tag }
+      used_tag_names = Set.new(tags.map &:tag)
+      unused_tag_names = used_tag_names - tag_names
+      unused_tag_names.each do |tag_name|
+        mapping_tags[tag_name].destroy
+      end
+      new_tag_names = tag_names - used_tag_names
+      new_tag_names.each do |tag_name|
+        @task.user_tags << make_tag(tag_name)
+      end
+    end
+
+    def get_tag_names(tags_line)
+      Set.new (tags_line.split(',').map &:strip).select {|v| v.size > 0}
+    end
+
+    def make_tag(text)
+      user_tag = UserTag.new
+      user_tag.user = current_user
+      user_tag.tag = text
+      user_tag.save
+      user_tag
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,9 @@ class Task < ActiveRecord::Base
   belongs_to :author, :class_name => 'User'
 
   has_and_belongs_to_many :user_tags
+
+  def tags_line
+    names = user_tags.map &:tag
+    names.join(', ')
+  end
 end
