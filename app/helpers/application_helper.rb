91e0c1bfdee9ff2b7129fb4dcf5553c65b041c94
ruyielder <ruyielder@gmail.com>
2014-11-26 17:40:19 +0100

    Added show errors when registration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def error_message!
+    return '' if resource.errors.empty?
+    field, error = resource.errors.first
+    message = "#{field} #{error}"
+
+    html = <<-HTML
+    <div class="alert alert-danger" role="alert">
+      <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+      <span class="sr-only">Error:</span>#{message}
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
 end
