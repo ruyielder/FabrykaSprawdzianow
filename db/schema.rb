3eb6e605ff3bcd365f88b805687f5955066a49eb
ruyielder <ruyielder@gmail.com>
2014-12-02 22:43:49 +0100

    Replaced sqlite3 with pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141201173446) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "student_task_tests", force: true do |t|
     t.integer  "student_task_id"
     t.integer  "test_id"
@@ -59,7 +62,7 @@ ActiveRecord::Schema.define(version: 20141201173446) do
     t.string   "title"
   end
 
-  add_index "tests", ["student_id"], name: "index_tests_on_student_id"
+  add_index "tests", ["student_id"], name: "index_tests_on_student_id", using: :btree
 
   create_table "user_tags", force: true do |t|
     t.integer  "user_id"
@@ -83,7 +86,7 @@ ActiveRecord::Schema.define(version: 20141201173446) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
